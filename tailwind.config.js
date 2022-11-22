791a1e9aebc39a364e92f3de14a9000277ab845b
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-22 20:50:12 -0300

    style(all): colors box shadow and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./src/shared/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "red-recording-main": "#FC3939",
+        "red-recording-dark": "#D80000",
+        "purple-stop-recording-main": "#3D39FC",
+     },  
+     boxShadow: {
+        "recording": "0px 0px 70px 40px #fc39394d",
+        "stop-recording": "0px 0px 70px 40px #3d39fc47",
+     },
+     spacing: {
+      "75vh": "75vh",
+    }
+    },
     fontFamily: {
       display: ["Manrope"]
     }
