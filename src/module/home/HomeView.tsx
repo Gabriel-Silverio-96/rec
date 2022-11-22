b72e835b7cabdeca584c210ffeb51bb46925e815
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-22 20:50:35 -0300

    feat(home): button recording

--- src/module/home/HomeView.tsx
+++ src/module/home/HomeView.tsx
@@ -1,5 +1,13 @@
+import { FaMicrophone } from "react-icons/fa";
+
 const HomeView = () => {
-	return <div>Home</div>;
+	return (
+		<div className="h-75vh flex items-center text-center justify-center flex-col">					
+			<button className="bg-indigo-500 w-72 h-72 flex items-center justify-center m-auto rounded-full bg-red-recording-main shadow-lg shadow-recording">
+				<FaMicrophone color="white" size={90} />
+			</button>
+		</div>
+	);
 };
 
 export default HomeView;
