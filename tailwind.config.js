19d948af0dd9ea21cf6ddc3f465ffddea2a136b6
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-22 19:26:45 -0300

    chore(config): tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/module/home/*.{js,jsx,ts,tsx}",
+    "./src/shared/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
