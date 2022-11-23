1073e981c4802fef2e681269ecc2fb2d9d3d985d
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-23 19:23:21 -0300

    refactor(home): recording class name

--- src/module/home/HomeView.tsx
+++ src/module/home/HomeView.tsx
@@ -1,10 +1,16 @@
 import { FaMicrophone } from "react-icons/fa";
+import { RiStopFill } from "react-icons/ri";
 
-const HomeView = () => {
+const HomeView: React.FC<any> = ({ isRecording, toggleRecording }) => {
+	const isRecordingClassName = isRecording ? "btn-main btn-stop-recording" : "btn-main btn-recording";
+	const isRecordingIcon = isRecording ? <RiStopFill color="white" size={90}/> : <FaMicrophone color="white" size={90} />;
+
 	return (
-		<div className="h-75vh flex items-center text-center justify-center flex-col">					
-			<button className="bg-indigo-500 w-72 h-72 flex items-center justify-center m-auto rounded-full bg-red-recording-main shadow-lg shadow-recording">
-				<FaMicrophone color="white" size={90} />
+		<div className="h-75vh flex items-center text-center justify-center flex-col">
+			<button className={isRecordingClassName}
+				onClick={toggleRecording}
+			>
+				{isRecordingIcon}				
 			</button>
 		</div>
 	);
