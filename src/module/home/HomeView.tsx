2829e778fbf2119797d01968d02e276f69d5d1be
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-26 19:14:00 -0300

    refactor(home): classes name

--- src/module/home/HomeView.tsx
+++ src/module/home/HomeView.tsx
@@ -1,6 +1,7 @@
 import { FaMicrophone } from "react-icons/fa";
 import { RiStopFill } from "react-icons/ri";
 import { IHomeView } from "./types/Home.component";
+import "./style/home.css";
 
 const HomeView: React.FC<IHomeView> = props => {
 	const {
@@ -11,7 +12,8 @@ const HomeView: React.FC<IHomeView> = props => {
 		audioURL,
 	} = props;
 
-	const isRecordingClassName = isRecording ? "btn-main btn-stop-recording" : "btn-main btn-recording";
+	const isRecordingClassName = isRecording ? "btn-stop-recording" : "btn-recording";
+	const btnMainClassesName = `btn-main ${isRecordingClassName}`;
 
 	const isRecordingIcon = isRecording ? (
 		<RiStopFill color="white" size={90} />
@@ -20,18 +22,18 @@ const HomeView: React.FC<IHomeView> = props => {
 	);
 
 	return (
-		<div className="h-75vh flex items-center text-center justify-center flex-col">
+		<div className="home-container">
 			<button
-				className={isRecordingClassName}
+				className={btnMainClassesName}
 				onClick={toggleRecording}
 				disabled={!microphoneAllowed}>
 				{isRecordingIcon}
 			</button>
 			{!microphoneAllowed && (
 				<button
-					className="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded mb-5"
+					className="btn-allowed-microphone"
 					onClick={requestAccessMicrophone}>
-					Allow use of microphone
+						Allow use of microphone
 				</button>
 			)}
 			<audio controls src={audioURL} />
