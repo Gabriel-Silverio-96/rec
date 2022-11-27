e66e08bc8b855a9ce41fc46a380680eb531db763
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-27 13:21:00 -0300

    refactor(home): remove unnecessary state

--- src/module/home/Home.tsx
+++ src/module/home/Home.tsx
@@ -1,48 +1,52 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import HomeView from "./HomeView";
 import { toast } from "react-toastify";
 
 const Home = () => {
-	const [microphoneAllowed, setMicrophoneAllowed] = useState(true);
-	const [requestMicrophone, setRequestMicrophone] = useState(false);
-
 	const [isRecording, setRecording] = useState(false);
+	const [microphoneAllowed, setMicrophoneAllowed] = useState(true);
+	
 	const [audioURL, setAudioURL] = useState("");
 	const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | any>();
-	
+
 	const dataavailableFn = (event: BlobEvent) => {
 		const { data } = event;
 		const BLOB_TYPE = "audio/ogg; codecs=opus";
-		const blob = new Blob([data], {	type: BLOB_TYPE });
+		const blob = new Blob([data], { type: BLOB_TYPE });
 
 		const createAudioURL = window.URL.createObjectURL(blob);
 		setAudioURL(createAudioURL);
 	};
-	
+
+	const recordAudio = useCallback(async () => {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({
+				audio: true,
+			});
+			const instanceMediaRecorder = new MediaRecorder(stream);
+
+			setMicrophoneAllowed(true);
+			instanceMediaRecorder.addEventListener(
+				"dataavailable",
+				dataavailableFn
+			);
+			setMediaRecorder(instanceMediaRecorder);
+		} catch (error) {
+			setMicrophoneAllowed(false);
+			toast.error("Permission to use microphone denied", {
+				toastId: "permission-microphone",
+			});
+			console.error(error);
+		}
+	}, []);
+
 	useEffect(() => {
-		const recordAudio = async () => {
-			try {
-				const stream =	await navigator.mediaDevices.getUserMedia({ audio: true });
-				const instanceMediaRecorder = new MediaRecorder(stream);
-
-				setMicrophoneAllowed(true);
-				instanceMediaRecorder.addEventListener("dataavailable",	dataavailableFn);
-				setMediaRecorder(instanceMediaRecorder);
-
-			} catch (error) {
-				setMicrophoneAllowed(false);
-				toast.error("Permission to use microphone denied",  {
-					toastId: "permission-microphone",
-				});
-				console.error(error);
-			}
-		};
 		recordAudio();
-	}, [requestMicrophone]);
+	}, []);
 
-	const requestAccessMicrophone = () => setRequestMicrophone(prevState => !prevState);
+	const requestAccessMicrophone = () => recordAudio();
 
-	const toggleRecording = async () => {		
+	const toggleRecording = async () => {
 		setRecording(prevState => !prevState);
 		if (!isRecording) {
 			toast.info("Start recording");
@@ -54,7 +58,17 @@ const Home = () => {
 		mediaRecorder.stop();
 	};
 
-	return <HomeView {...{ microphoneAllowed, isRecording, requestAccessMicrophone, toggleRecording, audioURL }} />;
+	return (
+		<HomeView
+			{...{
+				microphoneAllowed,
+				isRecording,
+				requestAccessMicrophone,
+				toggleRecording,
+				audioURL,
+			}}
+		/>
+	);
 };
 
 export default Home;
