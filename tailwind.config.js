0aacf4e6d787aedc700bb420bbe59e5c5b1308cc
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-12-07 19:50:38 -0300

    style(all): change colors box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,10 @@ module.exports = {
         "stop-recording-main": "#FC3939",
      },  
      boxShadow: {
-        "recording": "0px 0px 70px 40px #fc39394d",
-        "recording-hover": "0px 0px 50px 10px #fc39394d",        
-        "stop-recording": "0px 0px 70px 40px #3d39fc47",
-        "stop-recording-hover": "0px 0px 50px 10px #3d39fc47",
+        "recording": "0px 0px 70px 40px #3d39fc47",
+        "recording-hover": "0px 0px 50px 10px #3d39fc47",        
+        "stop-recording": "0px 0px 70px 40px #fc39394d",
+        "stop-recording-hover": "0px 0px 50px 10px #fc39394d",
      },
      spacing: {
       "75vh": "75vh",
